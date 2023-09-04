3a2d74234a2ae99bef619c092ae993413082e341
davidecarizzoni <davide.carizzoni@runelab.it>
2023-09-04 11:17:10 +0200

    chore(clean)

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,9 +1,8 @@
-import { useMutation, useQuery, useInfiniteQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import { appAxios, pickData } from './config';
 import { User } from '../types';
 import { useDispatch } from 'react-redux';
 import { setUserDetail } from '../store/user';
-import axios from 'axios';
 
 export const useGetUser = () => {
   const dispatch = useDispatch();
@@ -23,23 +22,6 @@ export const useUpdateUser = () => {
     mutationFn: (data: Partial<User>) => appAxios.put<User>('users/me', data).then(pickData),
     onSuccess: data => {
       dispatch(setUserDetail(data));
-    },
-  });
-};
-
-export const useGetUsers = () => {
-  const queryKey = ['users'];
-  const getUsers = async ({ pageParam = 1 }) => {
-    const { data } = await appAxios.get<User[]>('users', { params: { page: pageParam, limit: 5 } });
-    return {
-      data: data,
-      nextPage: pageParam + 1,
-    };
-  };
-  return useInfiniteQuery(queryKey, getUsers, {
-    getNextPageParam: ({ nextPage, data }) => {
-      if (data.length < 5) return undefined;
-      return nextPage;
     },
   });
 };
